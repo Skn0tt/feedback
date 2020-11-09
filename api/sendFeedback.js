3a40d91d2c3fd4cd970c3ebc1b18a2ad26d85897
Simon Knott <info@simonknott.de>
2020-11-09 18:28:46 +0100

    Format Subject nicer

--- api/sendFeedback.js
+++ api/sendFeedback.js
@@ -4,10 +4,16 @@ const client = new postmark.ServerClient(process.env.POSTMARK_TOKEN);
 
 export default async function sendFeedback(req, res) {
   const { topic, feedback } = req.body;
+
+  let Subject = "You got new Feedback";
+  if (topic) {
+    Subject += " on " + topic;
+  }
+
   await client.sendEmail({
     From: "feedback@simonknott.de",
     To: "feedback@simonknott.de",
-    Subject: "You got new Feedback on " + topic,
+    Subject,
     TextBody: feedback,
   });
   res.status(200).end();
